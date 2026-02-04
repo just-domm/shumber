88018e9533335bcf196c172bbe21d13933cace21
sudo-warrior <lawrencewafula408@gmail.com>
2026-02-04 12:48:46 +0000

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Shumber - Uber for Harvests",
   description: "Uber for Harvests",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
